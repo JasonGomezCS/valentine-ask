511bac1cf18b0d210cff4654c7abadf59c21ce89
Jason Gomez <gomezjd1210@student.laccd.edu>
2026-02-13 12:22:29 -0800

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ noBtn.addEventListener("mouseover", () => {
 // YES is clicked
 
 yesBtn.addEventListener("click", () => {
-    title.textContent = "Yippeeee!";
+    title.textContent = "I Like You So Much Kassandra";
 
     catImg.src = "cat_dance.gif";
 
